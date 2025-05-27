3494c6a75ffc93654ffda617161a784ddb0dcf13
Damaris34 <oliveiradamares211@gmail.com>
2025-05-27 14:53:30 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,17 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const formattedDate = today.toISOString().split('T')[0];
     document.getElementById('data-registro').value = formattedDate;
 
-    // Gerar PDF
-    const { jsPDF } = window.jspdf;
-    const doc = new jsPDF();
+    document.getElementById('generate-pdf').addEventListener('click', function() {
+        const { jsPDF } = window.jspdf;
+        const doc = new jsPDF();
 
-    doc.html(document.querySelector('.container'), {
-        callback: function(doc) {
-            doc.save('controle-cloro-livre.pdf');
-        },
-        x: 10,
-        y: 10,
-        width: 190,
-        windowWidth: 800
+        doc.html(document.querySelector('.container'), {
+            callback: function(doc) {
+                doc.save('controle-cloro-livre.pdf');
+            },
+            x: 10,
+            y: 10,
+            width: 190,
+            windowWidth: 800
+        });
     });
 });
