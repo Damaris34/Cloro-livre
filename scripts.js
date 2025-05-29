fe380050c55990304c37b02e1f1ecff8a9e08fd4
Damaris34 <oliveiradamares211@gmail.com>
2025-05-29 14:55:58 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,1 @@
-document.querySelectorAll('.file-input').forEach(input => {
-    input.addEventListener('change', function(e) {
-        const fileName = e.target.files[0] ? e.target.files[0].name : 'Nenhum arquivo escolhido';
-        e.target.nextElementSibling.textContent = fileName;
-    });
-});
+
