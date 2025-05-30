3a44caec66dccc6e99ff8d0c98fdcccc16bffd9d
Damaris34 <oliveiradamares211@gmail.com>
2025-05-30 14:35:01 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,5 @@ document.getElementById('increase').addEventListener('click', function() {
 
 document.getElementById('decrease').addEventListener('click', function() {
     let currentLevel = parseFloat(document.getElementById('current-level').value) || 0;
-    if (currentLevel > 0) {
-        document.getElementById('current-level').value = (currentLevel - 0.1).toFixed(1) + ' mg/L';
-    }
+    document.getElementById('current-level').value = (currentLevel - 0.1).toFixed(1) + ' mg/L';
 });
-
