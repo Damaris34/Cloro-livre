e3ade20b458f72db2b7939bc514784ba42894752
Damaris34 <oliveiradamares211@gmail.com>
2025-05-30 14:24:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-document.getElementById('increase').addEventListener('click', function() {
-    let currentLevel = parseFloat(document.getElementById('current-level').value) || 0;
-    document.getElementById('current-level').value = (currentLevel + 0.1).toFixed(1) + ' mg/L';
-});
 
-document.getElementById('decrease').addEventListener('click', function() {
-    let currentLevel = parseFloat(document.getElementById('current-level').value) || 0;
-    if (currentLevel > 0) {
-        document.getElementById('current-level').value = (currentLevel - 0.1).toFixed(1) + ' mg/L';
-    }
-});
